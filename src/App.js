1074786cf9bf6b976aa418d9a667771deb4f52a0
Denis Poplavskii <normal64@mail.ru>
2022-12-01 13:43:53 +0100

    feat: main logic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,63 @@
-import React from "react";
-import "./styles/App.scss"
+import React, { useState } from "react";
+import "./styles/App.scss";
 const App = () => {
+  const [PLAYER_SCORE, setPLAYER_SCORE] = useState(0);
+  const WINNER_COMBO = [
+    {
+      selected: "rock",
+      beats: "scissors",
+      icon: "✊",
+    },
+    {
+      selected: "paper",
+      beats: "rock",
+      icon: "✋",
+    },
+    {
+      selected: "scissors",
+      beats: "paper",
+      icon: "✌",
+    },
+  ];
+  function handlePlayersChoice(e, choiceNumber) {
+    console.log(e.target.id);
+    console.log(choiceNumber);
+    calculateWinner(WINNER_COMBO[choiceNumber], ai_selected());
+  }
+  function ai_selected() {
+    const RandomIndex = Math.floor(Math.random() * WINNER_COMBO.length);
+    return WINNER_COMBO[RandomIndex];
+  }
+  function calculateWinner(user, ai) {
+    if (user.beats == ai.selected) {
+      setPLAYER_SCORE(PLAYER_SCORE + 1);
+      console.log(`user.icon`, user.icon);
+      updateUI(user.icon, ai.icon, "You won", user, ai);
+      return;
+    }
+    if (user.selected == ai.selected) {
+      updateUI(user.icon, ai.icon, "Draw", user, ai);
+      return;
+    }
+    updateUI(user.icon, ai.icon, "You lost", user, ai);
+  }
+  function updateUI(userIcon, aiIcon, outcome_text, user, ai) {
+    document.querySelector(".user-results").innerHTML = userIcon;
+    document.querySelector(".user-results").classList.add(`${user.selected}`);
+
+    document.querySelector(".ai-results").innerHTML = aiIcon;
+    document.querySelector(".ai-results").classList.add(`${ai.selected}`);
+
+    document.querySelector("#won-text").innerHTML = outcome_text;
+    document.querySelector(".game-container").classList.add("hide");
+    document.querySelector(".outcome").classList.remove("hide");
+    document.querySelector(".winner-container").classList.remove("hide");
+  }
+  function resetGame() {
+    document.querySelector(".game-container").classList.remove("hide");
+    document.querySelector(".winner-container").classList.add("hide");
+    document.querySelector(".outcome").classList.add("hide");
+  }
   return (
     <div>
       <div className="container">
@@ -12,27 +69,47 @@ const App = () => {
           </h2>
           <div className="score-container">
             <p>score</p>
-            <div id="score">1</div>
+            <div id="score">{PLAYER_SCORE}</div>
           </div>
         </div>
         <div className="game-container">
-            <div id="rock" className="selectable rock">✊</div>
-            <div id="paper" className="selectable paper">✋</div>
-            <div id="scissors" className="selectable scissors">✌</div>
+          <div
+            id="rock"
+            className="selectable rock"
+            onClick={(e, choice = 0) => handlePlayersChoice(e, choice)}
+          >
+            ✊
+          </div>
+          <div
+            id="paper"
+            className="selectable paper"
+            onClick={(e, choice = 1) => handlePlayersChoice(e, choice)}
+          >
+            ✋
+          </div>
+          <div
+            id="scissors"
+            className="selectable scissors"
+            onClick={(e, choice = 2) => handlePlayersChoice(e, choice)}
+          >
+            ✌
+          </div>
         </div>
         <div className="winner-container hide">
-            <div className="selected">
-                <div className="user-results"></div>
-                <p>You Picked</p>
-            </div>
-            <div className="selected">
-                <div className="ai-results"></div>
-                <p>AI Picked</p>
-            </div>
+          <div className="selected">
+            <div className="user-results"></div>
+            <p>You Picked</p>
+          </div>
+          <div className="selected">
+            <div className="ai-results"></div>
+            <p>AI Picked</p>
+          </div>
         </div>
-        <div className="outcome">
-            <h2 id="won-text"></h2>
-            <button id="reset">Play again</button>
+        <div className="outcome hide">
+          <h2 id="won-text"></h2>
+          <button id="reset" onClick={resetGame}>
+            Play again
+          </button>
         </div>
       </div>
     </div>
